70f8230d489c9b0d17bdbef6a2e4a8b8f43c6a72
Patrick Alexander <patrick_alexander@mail.com>
2019-09-17 04:09:45 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ quotient = num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(argue)
+  argue
 end
 
-def greeting
-
+def greeting(name,language)
+  puts "Hello #{name}! You are learning #{language}!"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(value="cheese")
+  value
 end
